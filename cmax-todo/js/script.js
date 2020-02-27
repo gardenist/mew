8bdde4c6964eea8403e7153851da3505075e5b15
bflat-kay <bflat.kay@gmail.com>
2020-02-27 22:16:11 +0900

    체크되면 done 이동, 체크 해제하면 todo로 이동, 입력란에서 엔터쳐도 저장됨

--- cmax-todo/js/script.js
+++ cmax-todo/js/script.js
@@ -6,10 +6,26 @@ var btnClick = function(){
   let input = document.createElement('input');
   input.type = "checkbox";
   input.id = "todo" + i;
-  input.onchange = function() {
-    alert(this.id); 
-  } 
   let label = document.createElement('label');
+  input.onchange = function() {
+    let doneList = document.querySelector("#doneContents ul");
+    if(this.checked) {
+        doneList.appendChild(li);
+        label.classList.add("done");
+    }
+    else {
+        todoList.appendChild(li);
+        label.classList.remove("done");
+    }
+
+/*  let li = document.createElement('li');
+    let label = document.createElement('label');
+    label.textContent = todoContent;
+    li.appendChild(label);
+    doneList.appendChild(li);
+    this.parentNode.remove(); */
+
+  } 
   label.textContent = todoContent;
   li.appendChild(input); 
   li.appendChild(label);
@@ -17,6 +33,7 @@ var btnClick = function(){
   i = i+1; 
   document.getElementById("content").value = '';
 }
+
 
 
  
