9764b16063b9887648130af68fb2083c30be3d3b
bflat-kay <bflat.kay@gmail.com>
2020-03-27 20:40:13 +0900

    감싸기 과제 완료

---
+++ homework/0324/wrap_function.js
@@ -0,0 +1,12 @@
+function wrap(text, start, end) {
+  // split으로 배열에 넣음 
+  let pool = text.split("\n");
+  // 각 요소의 앞, 뒤에 start랑 end를 넣음 
+  for(var i=0; i < pool.length; i++) {
+      pool[i] = start + pool[i] + end;
+      console.log(pool[i]);
+  };
+  // 다시 구분자로 배열 합침 
+  let result = pool.join("\n");
+  return result;
+};
